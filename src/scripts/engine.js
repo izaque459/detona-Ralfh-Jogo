abbbd9a9cfec0670f25a774823497908bdc38a41
Isaac_Santos <isaacdsantos@hotmail.com>
2023-11-13 16:48:23 -0300

    Corrigido e atualizado addListenerHitBox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,15 @@
 const state = {
     view:{
         squares: document.querySelectorAll(".square"),
-        enemy: document.querySelectorAll(".enemy"),
-        timeLeft: document.querySelectorAll("#time-left"),
-        score: document.querySelectorAll("#socre"), 
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"), 
      },
     values:{ 
         timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 };
 
@@ -21,6 +23,8 @@ function randomSquares(){
     let randomSquare = state.view.squares[randomNumber];
 
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+
 }
 
 function moveEnemy(){
@@ -29,12 +33,20 @@ function moveEnemy(){
 
 function addListenerHitBox(){
     state.view.squares.forEach((square )=> {
-        
+        square.addEventListener("mousedown", ()=>{
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+
+        });
     });
 }
 
 function initialize(){
     moveEnemy();
+    addListenerHitBox();
 }
 
 initialize();
